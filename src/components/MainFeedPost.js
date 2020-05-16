16c3bd0bcac5c288e2dd1d9a35867ed4c705d3b7
Lisa Kang <lleekkang@gmail.com>
2020-05-15 23:02:22 -0400

    Like display implementation 1

--- src/components/MainFeedPost.js
+++ src/components/MainFeedPost.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import Card from "@material-ui/core/Card";
@@ -6,6 +6,10 @@ import CardMedia from "@material-ui/core/CardMedia";
 import { CardContent } from "@material-ui/core";
 import { apiBaseUrl } from "../config";
 import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import { UserContext } from "../UserContext";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -37,12 +41,54 @@ const useStyles = makeStyles((theme) => ({
   postContainer: {
     paddingTop: 30,
   },
+  notLiked: {
+    color: "black",
+  },
+  liked: {
+    color: "red",
+  },
 }));
 
 const MainFeedPost = (props) => {
   const classes = useStyles();
+  const postId = props.post.id;
+  //   const [likes, setLikes] = useState([]);
+  const [liked, setLiked] = useState(false);
+  const { userId } = useContext(UserContext);
+
+  useEffect(() => {
+    const getLikes = async () => {
+      const response = await fetch(`${apiBaseUrl}/posts/${postId}/likes`);
+      if (response.ok) {
+        const { userIds } = await response.json();
+        if (userIds.includes(Number(userId))) {
+          setLiked(true);
+        } else {
+          setLiked(false);
+        }
+      }
+    };
+    getLikes();
+  }, []);
+
+  //   const handleLike = async () => {
+  //     if (!liked) {
+  //         const response = await fetch(`${apiBaseUrl}/posts/${postId}/likes`, {
+  //             method: "POST",
+  //             headers: { 'Content-Type': "application/json"},
+  //             body: JSON.stringify({userId})
+  //         });
+  //         if (response.ok) {
+  //             setLiked(true);
+  //         }
+  //     } else {
+  //         const response = await fetch()
+  //         setLiked(false);
+  //     }
+  //   };
+
   return (
-    <div className={classes.postContainer}>
+    <div key={props.post.id} className={classes.postContainer}>
       <Card className={classes.root}>
         <CardContent className={classes.userInfo}>
           <Avatar className={classes.avatar} src={props.post.User.imageUrl} />
@@ -53,7 +99,16 @@ const MainFeedPost = (props) => {
           image={props.post.imageUrl}
         ></CardMedia>
 
-        <CardContent></CardContent>
+        <CardContent>
+          <IconButton>
+            {liked ? (
+              <FavoriteIcon className={classes.liked} />
+            ) : (
+              <FavoriteBorderIcon className={classes.notLiked} />
+            )}
+          </IconButton>
+          <div></div>
+        </CardContent>
       </Card>
     </div>
   );
