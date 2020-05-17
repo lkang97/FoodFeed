bb9f084313f5687a729307b52422b886dcb08d0b
Lisa Kang <lleekkang@gmail.com>
2020-05-16 23:07:54 -0400

    Add create new post feature

---
+++ src/components/NewPostForm.js
@@ -0,0 +1,181 @@
+import React, { useState, useContext } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { TextField, Button } from "@material-ui/core/";
+import AddAPhotoIcon from "@material-ui/icons/AddAPhoto";
+import { apiBaseUrl } from "../config";
+import { UserContext } from "../UserContext";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import InputLabel from "@material-ui/core/InputLabel";
+import IconButton from "@material-ui/core/IconButton";
+import ImageIcon from "@material-ui/icons/Image";
+
+const useStyles = makeStyles((theme) => ({
+  formContainer: {
+    display: "flex",
+    justifyContent: "center",
+    justifySelf: "center",
+    border: "1px solid grey",
+    boxShadow: "0px 11px 35px 2px rgba(0, 0, 0, 0.14)",
+    width: "500px",
+    height: "auto",
+    borderRadius: "1.2em",
+  },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    paddingBottom: 20,
+    width: "500px",
+  },
+  inputFields: {
+    paddingBottom: "10px",
+    paddingLeft: 20,
+    width: 400,
+  },
+  buttonContainer: {
+    padding: "10px",
+  },
+  button: {
+    width: "200px",
+  },
+  inputGroup: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    paddingBottom: 10,
+  },
+  inputLabels: {
+    display: "flex",
+    alignSelf: "center",
+    justifyContent: "flex-start",
+    color: theme.palette.primary.main,
+    fontWeight: "bold",
+  },
+  preview: {
+    width: 300,
+    height: 300,
+  },
+  image: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingBottom: 20,
+  },
+  uploadButton: {
+    height: 50,
+    alignSelf: "center",
+  },
+}));
+
+const NewPostForm = () => {
+  const classes = useStyles();
+  const { userId, authToken } = useContext(UserContext);
+  const [imageUrl, setImageUrl] = useState();
+  const [caption, setCaption] = useState();
+  const [isUpdated, setIsUpdated] = useState(false);
+  const [file, setFile] = useState();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch(`${apiBaseUrl}/posts`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${authToken}`,
+      },
+      body: JSON.stringify({
+        imageUrl,
+        caption,
+        userId,
+      }),
+    });
+    if (response.ok) {
+      setIsUpdated(true);
+    }
+  };
+
+  const uploadImage = async (e) => {
+    console.log(e.target.files);
+    const formData = new FormData();
+    formData.append("image", e.target.files[0]);
+    setFile(e.target.files[0]);
+    const image = formData.get("image");
+    console.log(image);
+
+    const response = await fetch(`${apiBaseUrl}/upload`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+      body: formData,
+    });
+    if (response.ok) {
+      const { imageUrl } = await response.json();
+      setImageUrl(imageUrl);
+    }
+  };
+  const updateCaption = (e) => setCaption(e.target.value);
+
+  if (isUpdated) window.location.reload();
+
+  return (
+    <div className={classes.formContainer}>
+      <form className={classes.form} onSubmit={handleSubmit}>
+        <div>
+          <h2>Create a new post</h2>
+        </div>
+        <div className={classes.image}>
+          <Card>
+            {imageUrl ? (
+              <CardMedia className={classes.preview}>
+                <img
+                  className={classes.preview}
+                  src={URL.createObjectURL(file)}
+                  alt="preview"
+                />
+              </CardMedia>
+            ) : (
+              <ImageIcon className={classes.preview} color="primary" />
+            )}
+          </Card>
+          <IconButton color="primary" className={classes.uploadButton}>
+            <input
+              accept="image/*"
+              style={{ display: "none" }}
+              type="file"
+              id="raised-button-file"
+              onChange={uploadImage}
+            ></input>
+            <label htmlFor="raised-button-file">
+              <AddAPhotoIcon variant="raised"></AddAPhotoIcon>
+            </label>
+          </IconButton>
+        </div>
+        <div className={classes.inputGroup}>
+          <InputLabel className={classes.inputLabels}>Caption</InputLabel>
+          <TextField
+            multiline
+            variant="outlined"
+            className={classes.inputFields}
+            color="primary"
+            value={caption}
+            onChange={updateCaption}
+          ></TextField>
+        </div>
+        <div className={classes.buttonContainer}>
+          <Button
+            className={classes.button}
+            color="primary"
+            variant="contained"
+            type="submit"
+          >
+            Submit
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default NewPostForm;
--- src/components/ProfileImages.js
+++ src/components/ProfileImages.js
@@ -6,6 +6,12 @@ import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import { useParams } from "react-router-dom";
 import { UserContext } from "../UserContext";
+import Button from "@material-ui/core/Button";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import { Typography } from "@material-ui/core";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import NewPostForm from "./NewPostForm";
 
 const useStyles = makeStyles((theme) => ({
   imagesContainer: {
@@ -13,11 +19,37 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "row-wrap",
     justifySelf: "center",
     justifyContent: "center",
-    paddingTop: 40,
+    paddingTop: 5,
     borderTop: "1px solid lightgray",
   },
   gridTiles: {
     justifyContent: "center",
+  },
+  newPost: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    paddingBottom: 15,
+  },
+  addIcon: {
+    height: 13,
+    width: 13,
+  },
+  addText: {
+    padding: 3,
+    fontSize: 13,
+  },
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "auto",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
   },
 }));
 
@@ -26,6 +58,7 @@ const ProfileImages = () => {
   const classes = useStyles();
   const { id } = useParams();
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState();
 
   useEffect(() => {
     const getUserPosts = async (id) => {
@@ -41,8 +74,36 @@ const ProfileImages = () => {
 
     getUserPosts(id);
   }, [id, authToken]);
+
+  const handleOpen = async () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <div className={classes.imagesContainer}>
+      <div className={classes.newPost}>
+        <Button onClick={handleOpen}>
+          <AddCircleIcon className={classes.addIcon} color="primary" />
+          <Typography className={classes.addText} color="primary">
+            New Post
+          </Typography>
+        </Button>
+        <Modal
+          className={classes.modal}
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{ timeout: 500 }}
+        >
+          <div className={classes.paper}>
+            <NewPostForm />
+          </div>
+        </Modal>
+      </div>
       <GridList cellHeight={330} cols={3}>
         {posts.map((post) => {
           return (
